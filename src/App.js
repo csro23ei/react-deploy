d05e24913035916b0d690560b9b07707561c3ed5
csro23ei <dante030724@gmail.com>
2025-06-30 23:35:57 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p >
+        Edit < code > src / App.js < /code> and save to reload. < /
+        p > <
+        a className = "App-link"
+        href = "https://reactjs.org"
+        target = "_blank"
+        rel = "noopener noreferrer" >
+        Down in the deep the dragon used to sleep!!!!
+        <
+        /a> < /
+        header > <
+        /div>
+    );
+}
+
+export default App;
